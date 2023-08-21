dea1ea2ec193fc4fe484f37335e327dbab577dc3
koiic <ibraismail1990@gmail.com>
2023-08-21 19:59:36 +0100

    fix text

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def generate_response(input_text):
 with st.form('my_form'):
     text = st.text_area('Enter text: ', 'What are the three key pieces of advice for learning how to code?')
     submitted = st.form_submit_button('Submit')
-    if not open_api_key.startswith('sk-'):
+    if not openai_api_key.startswith('sk-'):
         st.warning('Please enter a valid OpenAI API key!', icon='⚠')
-    if submitted and open_api_key.startswith('sk-'):
+    if submitted and openai_api_key.startswith('sk-'):
         generate_response(text)
