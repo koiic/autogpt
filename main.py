ab2ca99ea5df565a0aa0922c2df65d72596edafb
koiic <ibraismail1990@gmail.com>
2023-08-21 20:01:48 +0100

    fix text

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ openai_api_key =  st.sidebar.text_input('OpenAI API Key', value='', type='passwo
 def generate_response(input_text):
    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key) 
    st.info('Generating response...')
+   st.info(llm(input_text))
    
    
 with st.form('my_form'):
